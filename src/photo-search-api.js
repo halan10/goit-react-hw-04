697913f636510ab34407d37df6a7a2fcfac84e42
halan10 <halanmykola@gmail.com>
2024-04-25 18:22:57 +0300

    fix api query

--- src/photo-search-api.js
+++ src/photo-search-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.unsplash.com/search/';
-const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+const CLIENT_ID = 'Q2hM1G5KyKyybz-9LTYPRyf9PfuatQNAioXNUYORBjE';
 
 export const fetchPhotos = async (searchQuery, currentPage) => {
   const responce = await axios.get('/photos', {
